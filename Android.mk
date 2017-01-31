a15c52d2c76c5006246d0cb5dfcbd92148a94a1a
hedwig34 <hedwig34@gmail.com>
2017-01-31 12:01:26 -0500

    OctOTA: Don't try to copy ota_conf. We already have it in place.
    Change-Id: I2b5f9a8fa7f1484dc56af3aca186298653bd1181

--- Android.mk
+++ Android.mk
@@ -1,9 +1,9 @@
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
-ifneq ($(ANDROID_BUILD_TOP),)
-    $(shell cp -u $(ANDROID_BUILD_TOP)/ota_conf $(LOCAL_PATH)/app/src/main/assets/)
-endif
+#ifneq ($(ANDROID_BUILD_TOP),)
+#    $(shell cp -u $(ANDROID_BUILD_TOP)/ota_conf $(LOCAL_PATH)/app/src/main/assets/)
+#endif
 
 LOCAL_MODULE_TAGS := optional
 LOCAL_PRIVILEGED_MODULE := true
